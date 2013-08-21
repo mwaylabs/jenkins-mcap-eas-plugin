986ac600700494ccd6c60ecbb47b1f7a2c05ccf3
Markus Pfeiffer <m.pfeiffer@mwaysolutions.com>
2013-08-21 12:22:23 +0200

    Added additional log output, set result to appropriate value in case an error occurs.

--- src/main/java/org/jenkinsci/plugins/relution/ApplicationFileCallable.java
+++ src/main/java/org/jenkinsci/plugins/relution/ApplicationFileCallable.java
@@ -22,7 +22,13 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 package org.jenkinsci.plugins.relution;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import hudson.FilePath.FileCallable;
 import hudson.Util;
@@ -31,113 +37,183 @@ import hudson.model.Result;
 import hudson.model.AbstractBuild;
 import hudson.remoting.VirtualChannel;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.UUID;
-
-import org.apache.http.ParseException;
 import org.apache.tools.ant.types.FileSet;
 import org.jenkinsci.plugins.relution.entities.ApplicationInformation;
 import org.jenkinsci.plugins.relution.entities.ShortApplicationInformation;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+
 @SuppressWarnings("serial")
 public class ApplicationFileCallable implements FileCallable<Boolean> {
 
-	@SuppressWarnings("rawtypes")
-	private final AbstractBuild build;
-	private final BuildListener listener;
-	private final Application application;
-	private final List<RelutionCommunicator> communicators;
-	
-	/**
-	 * Constructor.
-	 * @param build Actual build number.
-	 * @param listener Receives events that happen during a build.
-	 * @param communicators List of all global specified communicators.
-	 * @param application Job that will be build.
-	 */
-	@SuppressWarnings("rawtypes")
-	public ApplicationFileCallable(final AbstractBuild build,
-			final BuildListener listener, final List<RelutionCommunicator> communicators,
-			final Application application) {
-		this.build = build;
-		this.listener = listener;
-		this.application = application;
-		this.communicators = communicators;
-	}
-	
-	/**
-	 * These method is responsible to get the file to upload against the relution store during the build process.
-	 * 
-	 * <p>
-	 * If the file could not be found the build will marked as failed!
-	 * After the file is found retrieving short Information of the file 
-	 * In the last steps different request's will be created to
-	 * - after creating the first request an APIObject File Object will be returned in the reponse
-	 * - in the second step an request will created to get an APIObject App
-	 *   - Returns the existing APIObject App if resource with package name (internalName) already exists.
-	 *   - Returns an error if APIObject Version with versionCode already exists.
-	 * - the last step will upload the APIObject App created in step 2
-	 * @param f Holds information about path the project relates to.
-	 * @param channel Represents a communication channel to the remote/local peer.
-	 * @return true if build executes fine, false else
-	 */
-	public Boolean invoke(final File f, final VirtualChannel channel) throws IOException, InterruptedException {
-		ShortApplicationInformation app = null;
-		final FileSet fileSet = Util.createFileSet(f, this.application.getApplicationFile());
-		
-		if (fileSet.getDirectoryScanner().getIncludedFilesCount() < 1) {
-			this.listener.getLogger().println("[Relution Publisher]: Error, no files to deploy");
-			this.build.setResult(Result.UNSTABLE);
+    private static final String              ERROR_VERSION_ALREADY_EXISTS = "Version already exists. Please delete the old one to upload the same version again.";
 
-			return false;
-		}
+    @SuppressWarnings("rawtypes")
+    private final AbstractBuild              build;
+    private final BuildListener              listener;
+    private final Application                application;
+    private final List<RelutionCommunicator> communicators;
 
-		final File applicationFile = new File(fileSet.getDirectoryScanner().getBasedir().getAbsolutePath() + File.separator + fileSet.getDirectoryScanner().getIncludedFiles()[0]);
-		try {
-			// -----------------------------------------------------------------
-			// if(this.application.getApplicationUUID() == null) {
-			// this.application.setApplicationUUID(UUID.randomUUID().toString());
-			// }
-			app = new ShortApplicationInformation(UUID.randomUUID().toString());
-			if (app == null || app.getUUID() == null) {
-				this.listener.getLogger().println("[Relution Publisher]: Could not obtain application uuid");
-				return false;
-			}
-			this.listener.getLogger().println("[Relution Publisher]: Obtained application UUID: " + app.getUUID());
-			for(int index = 0; index < communicators.size(); index ++) {
-				RelutionCommunicator communicator = communicators.get(index);
-				if(communicator.getRequestFactory().getRelutionApiUrl().equals(this.application.getApiEndpointURL())) {
-					// app = communicator.getShortApplicationInformation(this.application.getApplicationUUID());
-					// -----------------------------------------------------------------
-					final String uploadedAssetToken = communicator.uploadApplicationAsset("", applicationFile);
-					// this.listener.getLogger().println("[Relution Publisher]: Uploaded application asset (token " + uploadedAssetToken + ")");
-					// -----------------------------------------------------------------
-					final String appObject = communicator.analyzeUploadedApplication(uploadedAssetToken, app.getUUID(), this.application.getApplicationIcon(), this.application.getApiReleaseStatus(), this.application.getApplicationName(), this.application.getApplicationReleaseNotes(), this.application.getApplicationDescription(), fileSet, communicator);
-					if(appObject.equals("Version already exists. Please delete the old one to upload the same version again.")) {
-						this.listener.getLogger().println("[Relution Publisher]: Version already exists. Please delete the old one to upload the same version again.");
-						return false;
-					}
-					// -----------------------------------------------------------------
-					final ApplicationInformation information = communicator.saveApplicationInformation(appObject);
-					// this.listener.getLogger().println("[Relution Publisher]: Obtained information for uploaded application asset ( " + information + " )");
-					this.listener.getLogger().println("[Relution Publisher]: Upload app with name " + applicationFile.getName());
-				}
-			}
-			return true;
-		} catch (final ParseException e) {
-			e.printStackTrace();
-		} catch (final URISyntaxException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+    /**
+     * Constructor.
+     * @param build Actual build number.
+     * @param listener Receives events that happen during a build.
+     * @param communicators List of all global specified communicators.
+     * @param application Job that will be build.
+     */
+    @SuppressWarnings("rawtypes")
+    public ApplicationFileCallable(final AbstractBuild build, final BuildListener listener, final List<RelutionCommunicator> communicators,
+            final Application application) {
 
-		return false;
-	}
+        this.build = build;
+        this.listener = listener;
+        this.application = application;
+        this.communicators = communicators;
+    }
 
+    /**
+     * Uploads the artifact (e.g. binary file) produced by a build process to the Relution store
+     * after the build process completes. If the specified {@link File} does not exist, the build
+     * is considered as <code>not built</code>.
+     * <p/>
+     * After existence of the file has been verified, a {@link ShortApplicationInformation} object
+     * will be retrieved. 
+     * <p>
+     * If the file could not be found the build will marked as failed!
+     * After the file is found retrieving short Information of the file 
+     * In the last steps different request's will be created to
+     * - after creating the first request an APIObject File Object will be returned in the reponse
+     * - in the second step an request will created to get an APIObject App
+     *   - Returns the existing APIObject App if resource with package name (internalName) already exists.
+     *   - Returns an error if APIObject Version with versionCode already exists.
+     * - the last step will upload the APIObject App created in step 2
+     * @param f Holds information about path the project relates to.
+     * @param channel Represents a communication channel to the remote/local peer.
+     * @return true if build executes fine, false else
+     */
+    @Override
+    public Boolean invoke(final File f, final VirtualChannel channel) throws IOException, InterruptedException {
+
+        try {
+            final FileSet fileSet = Util.createFileSet(f, this.application.getApplicationFile());
+
+            // If the file does not exist, consider the build as "not built" 
+            if (fileSet.getDirectoryScanner().getIncludedFilesCount() < 1) {
+                this.log("The configured application file does not exist, no files to deploy");
+                this.build.setResult(Result.NOT_BUILT);
+                return false;
+            }
+
+            final File applicationFile = this.getApplicationFile(fileSet);
+
+            this.log("Retrieving communicator for configured URI '%s'...", this.application.getApiEndpointURL());
+            final RelutionCommunicator communicator = this.getCommunicator(this.application);
+
+            if (communicator == null) {
+                this.log("Failed to retrieve communicator.");
+                this.build.setResult(Result.FAILURE);
+                return false;
+            }
+            this.log("Communicator retrieved, will upload to %s", communicator.getRequestFactory().getRelutionApiUrl());
+
+            this.log("Preparing to deploy '%s', retrieving application UUID...", applicationFile.getName());
+            final ShortApplicationInformation app = new ShortApplicationInformation(UUID.randomUUID().toString());
+
+            if (app == null || app.getUUID() == null) {
+                this.log("Failed to obtain application UUID");
+                this.build.setResult(Result.FAILURE);
+                return false;
+            }
+            this.log("Obtained application UUID {%s}", app.getUUID());
+
+            this.log("Upload application asset and retrieve token...");
+            final String uploadedAssetToken = communicator.uploadApplicationAsset("", applicationFile);
+
+            if (uploadedAssetToken == null || uploadedAssetToken.length() == 0) {
+                this.log("Upload failed, returned token is empty");
+                this.build.setResult(Result.FAILURE);
+                return false;
+            }
+            this.log("Token {%s} for application asset acquired", uploadedAssetToken);
+
+            this.log("Retrieving application object...");
+            final String appObject = communicator.analyzeUploadedApplication(
+                    uploadedAssetToken,
+                    app.getUUID(),
+                    this.application.getApplicationIcon(),
+                    this.application.getApiReleaseStatus(),
+                    this.application.getApplicationName(),
+                    this.application.getApplicationReleaseNotes(),
+                    this.application.getApplicationDescription(),
+                    fileSet,
+                    communicator);
+
+            if (appObject == null || appObject.length() == 0) {
+                this.log("Failed to retrieve application object.");
+                this.build.setResult(Result.FAILURE);
+                return false;
+            }
+
+            if (appObject.equals(ERROR_VERSION_ALREADY_EXISTS)) {
+                this.log("A file with the same version already exists. You must remove this version before you can upload it again.");
+                this.build.setResult(Result.UNSTABLE);
+                return false;
+            }
+
+            final JsonParser parser = new JsonParser();
+            final JsonElement element = parser.parse(appObject);
+            final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            this.log("Application object retrieved:\n%s", gson.toJson(element));
+
+            this.log("Saving application information...");
+            final ApplicationInformation information = communicator.saveApplicationInformation(appObject);
+
+            if (information == null) {
+                this.log("Failed to save application information.");
+                this.build.setResult(Result.FAILURE);
+                return false;
+            }
+            this.log("Application information saved");
+            this.build.setResult(Result.SUCCESS);
+            return true;
+
+        } catch (final Exception e) {
+            this.build.setResult(Result.FAILURE);
+            this.log(e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private File getApplicationFile(final FileSet fileSet) {
+
+        final String dir = fileSet.getDirectoryScanner().getBasedir().getAbsolutePath();
+        final String name = fileSet.getDirectoryScanner().getIncludedFiles()[0];
+        final String path = String.format("%s%s%s", dir, File.separator, name);
+
+        return new File(path);
+    }
+
+    private RelutionCommunicator getCommunicator(final Application application) {
+
+        for (int index = 0; index < this.communicators.size(); index++) {
+            final RelutionCommunicator communicator = this.communicators.get(index);
+
+            if (communicator.getRequestFactory().getRelutionApiUrl().equals(application.getApiEndpointURL())) {
+                return communicator;
+            }
+        }
+        return null;
+    }
+
+    private void log(final String format, final Object... args) {
+
+        final String text = String.format(format, args);
+        final String message = String.format("[Relution Publisher] %s", text);
+        this.listener.getLogger().println(message);
+    }
 }
